6241d0c76589423a302b3a96be96bff64a124913
Vinicius Okamoto <leo_kamoto@hotmail.com>
2020-05-30 19:41:06 -0300

    Projection, Lot , Product and Sale finished

--- src/model/Sale.java
+++ src/model/Sale.java
@@ -1,5 +1,51 @@
 package model;
 
-public class Sale {
+import java.util.Date;
 
+public class Sale {
+	private Lot lot;
+	private int qtOut;
+	private double total;
+	private Date date;
+	
+	public Sale(Lot lot, int qtOut, double total) {
+		this.lot = lot;
+		this.qtOut = qtOut;
+		this.total = total;
+		this.date = new Date();
+	}
+
+	public Lot getLot() {
+		return lot;
+	}
+
+	public void setLot(Lot lot) {
+		this.lot = lot;
+	}
+
+	public int getQtOut() {
+		return qtOut;
+	}
+
+	public void setQtOut(int qtOut) {
+		this.qtOut = qtOut;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
 }
